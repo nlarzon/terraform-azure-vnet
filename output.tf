697f6fa3d78ac55c5c81f766955ee8aa2300a47a
Steven B <genseb7@gmail.com>
2019-10-04 12:12:36 -0400

    conditionally output single object

--- output.tf
+++ output.tf
@@ -5,12 +5,12 @@ output "vnet" {
 
 output "resource_group" {
   description = "Resource group for VNET"
-  value       = azurerm_resource_group.rg
+  value       = length(azurerm_resource_group.rg) > 0 ? azurerm_resource_group.rg[0] : []
 }
 
 output "ddos_protection_plan" {
   description = "Ddos protection plan"
-  value       = azurerm_network_ddos_protection_plan.ddos
+  value       = length(azurerm_network_ddos_protection_plan.ddos) > 0 ? azurerm_network_ddos_protection_plan.ddos[0] : []
 }
 
 output "subnets" {
